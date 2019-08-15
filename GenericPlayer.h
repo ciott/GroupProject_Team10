3eaede9e3ffad01f5e7823195246b1c8297eb5f3
JamesMcPeek <50390312+JamesMcPeek@users.noreply.github.com>
2019-08-15 15:21:16 -0400

    Update GenericPlayer.h

--- GenericPlayer.h
+++ GenericPlayer.h
@@ -22,13 +22,13 @@ public:
     virtual ~GenericPlayer() {
     }
 
-    virtual bool IsHitting() const = 0; //Whether CPU keeps hitting
+    virtual bool IsHitting() const = 0; //Whether NPC keeps hitting
 
-    bool IsBusted() const{ //CPU hand total greater than 21
+    bool IsBusted() const{ //NPC hand total greater than 21
     return (getTotal() > 21);
     }
 
-    void Bust() const{ //CPU busts
+    void Bust() const{ //NPC busts
     cout << m_Name << " busted.\n";
     }
 
@@ -36,21 +36,21 @@ protected:
     string m_Name;
 };
 
-CPU strategy
-If ((dealer card 1 == 2 || dealer card 1 == 3)) && ((cpu handtotal >= 2 && cpu handottal <=12))
-    CPU hits
+NPC strategy
+If ((dealer card 1 == 2 || dealer card 1 == 3)) && ((npc handtotal >= 2 && npc handottal <=12))
+    NPC hits
 
-If ((dealer card 1 == 2 || dealer card 1 == 3)) && ((cpu handtotal >=13 && cpu handtotal <= 20))
-    CPU stands 
+If ((dealer card 1 == 2 || dealer card 1 == 3)) && ((npc handtotal >=13 && npc handtotal <= 20))
+    NPC stands 
 
-If ((dealer card 1 == 4 || dealer card 1 == 5 || dealer card 1 == 6)) && ((cpu handtotal >= 2 && cpu handtotal <= 11))
-	CPU hits
+If ((dealer card 1 == 4 || dealer card 1 == 5 || dealer card 1 == 6)) && ((npc handtotal >= 2 && npc handtotal <= 11))
+	NPC hits
 
-If ((dealer card 1 == 4 || dealer card 1 == 5 || dealer card 1 == 6)) && ((cpu handtotal >= 12 && cpu handtotal <= 20))
-	CPU stands
+If ((dealer card 1 == 4 || dealer card 1 == 5 || dealer card 1 == 6)) && ((npc handtotal >= 12 && npc handtotal <= 20))
+	NPC stands
 
-If ((dealer card 1 == 7 || dealer card 1 == 8 || dealer card 1 == 9) || dealer card 1 == 10 || dealer card 1 == A)) && ((cpu handtotal >= 2 && cpu handtotal <= 16))
-	CPU hits
+If ((dealer card 1 == 7 || dealer card 1 == 8 || dealer card 1 == 9) || dealer card 1 == 10 || dealer card 1 == A)) && ((npc handtotal >= 2 && npc handtotal <= 16))
+	NPC hits
 
-If ((dealer card 1 == 7 || dealer card 1 == 8 || dealer card 1 == 9) || dealer card 1 == 10 || dealer card 1 == A)) && ((cpu handtotal >= 17 && cpu handtotal <= 20))
-	CPU stands
+If ((dealer card 1 == 7 || dealer card 1 == 8 || dealer card 1 == 9) || dealer card 1 == 10 || dealer card 1 == A)) && ((npc handtotal >= 17 && npc handtotal <= 20))
+	NPC stands
